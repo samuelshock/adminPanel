147d2868538749c6c0559bbdd22cd693be0ebdd6
Unknown <samuel.choque@jalasoft.com>
2018-02-21 21:50:29 -0400

    Inputs and Outputs
    fin de la practica

--- src/app/components/graphic-doughnut/graphic-doughnut.component.ts
+++ src/app/components/graphic-doughnut/graphic-doughnut.component.ts
@@ -9,9 +9,9 @@ export class GraphicDoughnutComponent implements OnInit {
 
   @Input('name') leyend: string = 'Leyend';
 
-  @Input('labels') public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  @Input('data') public doughnutChartData: number[] = [350, 450, 100];
-  @Input('chartType') public doughnutChartType: string = 'doughnut';
+  @Input() chartLabels: string[] = [];
+  @Input() chartData: number[] = [];
+  @Input() chartType: string = '';
 
   constructor() { }
 
